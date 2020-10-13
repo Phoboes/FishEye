68e5e33e170045abfbb2415eb161862ed52c6c44
Kane <Phoboes@live.com>
2020-10-13 23:31:12 +1100

    Lifecycles are the worst.

--- src/components/Map/DiveSite/DiveSite.js
+++ src/components/Map/DiveSite/DiveSite.js
@@ -6,8 +6,9 @@ import DiveForm from '../../forms/DiveForm/DiveForm';
     const diveData = (
         <div>
           <h4>{props.name}</h4>
-          <p>Average vis: { props.vis }m</p>
-          <p>Last updated: { props.lastUpdated }</p>
+          <p>{props.description}</p>
+          {/* <p>Average vis: { props.vis }m</p> */}
+          {/* <p>Last updated: { props.lastUpdated }</p> */}
           <button onClick={ ()=>{ props.clicked( (<DiveForm/>) ); }}>+</button>
         </div>
       )
--- src/components/Map/Polygons/Polygons.js
+++ src/components/Map/Polygons/Polygons.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Polygon } from 'react-leaflet';
+import Popup from '../Popup/Popup'
+import DiveSite from '../DiveSite/DiveSite'
 
-
+  let popup = null;
   const Polygons = ( props ) => {
-    console.log("New polygon!");
+    const [ popupShow, setPopupState ] = useState({ show: false, coords: {} });
+    const [ siteData, setSiteData ] = useState({});
+
+    useEffect(()=>{
+      setSiteData({... props.siteData})
+    }, [siteData.name]);
+
+    useEffect(() => {
+      if( popupShow ){
+        popup = (
+          <Popup setLatLng={ popupShow.coords }>
+            <DiveSite name={siteData.name} description={siteData.description} />
+          </Popup>
+        );
+      } else {
+        popup = null;
+      }
+    }, [popupShow])
+
+    const popupHandler = (e) => {
+      // debugger
+      // setSiteData({...props.siteData})
+      setPopupState( { show: !popupShow, coords: e.latlng } );
+    };
+
+    // console.log("New polygon!");
       return (
-            <Polygon siteData={props.siteData} color={props.color} positions={props.siteData.boundaryPoints} onClick={(e)=>{}} />
+            <Polygon siteData={props.siteData} color={props.color} positions={props.siteData.boundaryPoints} onClick={popupHandler} >
+              { popup }
+            </Polygon>
       );
   }
 
